62ee86cbc3381e89ef4d652002eb56a8df1ee736
Mashopy <eliasgheeraert@gmail.com>
2024-02-20 19:24:37 +0100

    hisi: modem: nv: Include int-ll64.h
    Change-Id: I97423b1e65bd99410e4dff4d19e7563e31b81346

--- drivers/hisi/modem/include/nv/tl/drv/mdrv_nv_comm_def.h
+++ drivers/hisi/modem/include/nv/tl/drv/mdrv_nv_comm_def.h
@@ -55,17 +55,7 @@ extern "C" {
 #endif
 #endif
 
-#ifndef __u8_defined
-#define __u8_defined
-typedef signed char          s8;
-typedef unsigned char        u8;
-typedef signed short         s16;
-typedef unsigned short       u16;
-typedef signed int           s32;
-typedef unsigned int         u32;
-typedef signed long long     s64;
-typedef unsigned long long   u64;
-#endif
+#include <asm-generic/int-ll64.h>
 
 /*****************************************************************************
  ??????       :  NV_FEMPIN_TO_GPIO_STRU
