9747de238c70370ef1fd14e0a80b2423c05384af
Giovanni Ricca <giovanniricca@proton.me>
2025-02-06 23:46:44 +0100

    include: hiview_selinux: Fixup include guard
    Change-Id: I15f59410f5404ad8253b8d9e0b0752eb1a8d7929
    Signed-off-by: Giovanni Ricca <giovanniricca@proton.me>

--- include/chipset_common/hiview_selinux/hiview_selinux.h
+++ include/chipset_common/hiview_selinux/hiview_selinux.h
@@ -1,5 +1,5 @@
-#ifndef HIVIEW_SELINUX_H
-#define HIVEIW_SELINUX_H
+#ifndef __HIVIEW_SELINUX_H__
+#define __HIVIEW_SELINUX_H__
 #include <linux/lsm_audit.h>
 
 #define CONFIG_HUAWEI_HIVIEW_SELINUX
@@ -7,4 +7,4 @@
 
 int hw_hiview_selinux_avc_audit(struct common_audit_data *cad);
 
-#endif
+#endif // __HIVIEW_SELINUX_H__
